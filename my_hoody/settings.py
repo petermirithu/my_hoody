322093bd293ab09f5342809e0c6b5ef573a52a99
petermirithu <pyra_m.k@yahoo.com>
2020-01-10 22:27:30 +0300

    make first migrations

--- my_hoody/settings.py
+++ my_hoody/settings.py
@@ -11,26 +11,29 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+MODE=config('MODE')
+SECRET_KEY=config('SECRET_KEY')
+DEBUG=config('DEBUG', default=False,cast=bool)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wwc9(rr60684_#e4t$b0obtlk8&yi1id4aara!()ilyb27i6(l'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'hood_app.apps.HoodAppConfig',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'bootstrap4',
+    'pyuploadcare',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -67,18 +70,45 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK={
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
+
+UPLOADCARE = {
+    'pub_key': 'b3b631fd1c4393d31795',
+    'secret': '787678858a63d855993d',
+}
+
 WSGI_APPLICATION = 'my_hoody.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if config('MODE')=='dev':    
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': '',
+        }
     }
-}
+else:
+    DATABASES={
+        'default':dj_database_url.config(
+            default=config('DATABASE_URL')            
+        )
+    }   
+    db_from_env=dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
+    ALOOWED_HOSTS=config('ALLOWED_HOSTS',cast=Csv())
+
+
 
 
 # Password validation
@@ -105,7 +135,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -118,3 +148,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'static')    
+]
+
+django_heroku.settings(locals())
