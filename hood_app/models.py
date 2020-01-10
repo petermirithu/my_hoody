7665a433ded175a8fc6b39ed8d44c528a2bd7753
petermirithu <pyra_m.k@yahoo.com>
2020-01-10 22:24:45 +0300

    add a profile model

--- hood_app/models.py
+++ hood_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from pyuploadcare.dj.models import ImageField
 
-# Create your models here.
+class profile(models.Model):
+  '''
+  class that defines how a user's profile data shall be saved
+  '''
+  user=models.OneToOneField(User, on_delete=models.CASCADE)
+  profile_pic=ImageField(blank=True,manual_crop='')
+  bio=models.CharField(max_length=50,blank=True)
+  contact=models.CharField(max_length=15,blank=True)
+  
+
+
