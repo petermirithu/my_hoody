4306280233693cefb328b194d4344c6ac2def2c5
petermirithu <pyra_m.k@yahoo.com>
2020-01-10 22:24:23 +0300

    register profiel signals

--- hood_app/apps.py
+++ hood_app/apps.py
@@ -3,3 +3,10 @@ from django.apps import AppConfig
 
 class HoodAppConfig(AppConfig):
     name = 'hood_app'
+
+    def ready(self):
+        '''
+        function tha registers a signal
+        '''
+        import hood_app.signals
+
