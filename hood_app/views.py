8288c420a17a8685be04473fa402c567ac286ae0
petermirithu <pyra_m.k@yahoo.com>
2020-01-10 22:25:54 +0300

    add view functions for home logout and profile page

--- hood_app/views.py
+++ hood_app/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required(login_url='/accounts/login/')
+def home(request):
+  '''
+  view function that renders the home page
+  '''
+  return render(request,'index.html')
+
+@login_required(login_url='/accounts/login/')
+def logout_request(request):
+  '''
+  view function that logs a user
+  '''
+  logout(request)
+  return redirect('home')
+
+@login_required(login_url='/accounts/login/')
+def user_profile(request):
+  '''
+  view function that renders the profile
+  '''
+  title=request.user.username
+  content={
+    'title':title
+  }
+  return render(request,'profile.html',content)
+
