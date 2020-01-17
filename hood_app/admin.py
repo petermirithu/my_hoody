c9bde47048f1ffd58998ff95a77064e13aae8918
petermirithu <pyra_m.k@yahoo.com>
2020-01-17 21:36:38 +0300

    register models

--- hood_app/admin.py
+++ hood_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(hoodscounty)
+admin.site.register(profile)
+admin.site.register(businesses)
+admin.site.register(departments)
+admin.site.register(hoodposts)
+
+
